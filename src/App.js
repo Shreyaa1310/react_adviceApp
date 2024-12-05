562ab9899fbb93cafd753b2ec3474325cc467e8a
Shreya <shreya.m@kynhood.com>
2024-12-05 17:12:06 +0530

    advice app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
+const App = () => {
+  const [advice, setAdvice] = useState('');
+
+  // Function to fetch advice
+  const fetchAdvice = async () => {
+    try {
+      const response = await axios.get('https://api.adviceslip.com/advice');
+      const { advice } = response.data.slip;
+      setAdvice(advice);
+    } catch (error) {
+      console.error("Error fetching advice:", error);
+    }
+  };
+
+  // Fetch advice on component mount
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="card">
+        <h1 className="heading">{advice}</h1>
+        <button className="button" onClick={fetchAdvice}>
+          <span>GIVE ME ADVICE!</span>
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
